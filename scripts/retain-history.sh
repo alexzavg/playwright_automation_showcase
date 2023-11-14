4f3a005d0b23d90b8c7fee1a1aa776b10846aab2
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2023-11-14 17:10:27 +0200

    Auto commit

--- scripts/retain-history.sh
+++ scripts/retain-history.sh
@@ -4,11 +4,13 @@
 DIR=$1
 
 # Navigate to the directory
-cd ..
-cd $DIR
+echo "Navigating to the $DIR directory..."
+cd "$DIR" || exit
 
 # Retain the latest 3 reports
+echo "Retaining the latest 3 reports..."
 ls -t | tail -n +4 | xargs rm -rf --
 
 # Go back to the root directory
+echo "Returning to the root directory..."
 cd ..
