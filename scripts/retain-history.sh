d1248a562c1a0bbe5c546e7b263370691238fa91
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2023-11-14 19:01:22 +0200

    Auto commit

---
+++ scripts/retain-history.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# The directory (stage or prod) is passed as an argument
+DIR=$1
+
+# Absolute path to the directory
+DIR_PATH="$GITHUB_WORKSPACE/$DIR"
+
+# Check if the directory exists
+if [ ! -d "$DIR_PATH" ]; then
+    echo "Directory $DIR_PATH does not exist."
+    exit 1
+fi
+
+# Navigate to the directory
+cd "$DIR_PATH" || exit
+
+# Retain the latest 3 reports
+ls -t | tail -n +4 | xargs rm -rf --
+
+# Go back to the root directory
+cd "$GITHUB_WORKSPACE" || exit
